049b901067863d8d0546ea7121133eb74fcc4bd2
Frans Siswanto <franssiswanto@gmail.com>
2025-02-03 10:21:49 +0700

    fix: removing api.sancaka completely

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,14 +23,10 @@ check_and_generate_cert() {
 }
 
 # Request certificates only if necessary
-# check_and_generate_cert "api.sancaka.monosolusi.com"
 check_and_generate_cert "grafana.tokosumatra.monosolusi.com"
 check_and_generate_cert "loki.tokosumatra.monosolusi.com"
 
 # Update Nginx configuration with existing certificates
-# sed -i "s|ssl_certificate     /etc/ssl/certs/ssl-cert-snakeoil.pem;|ssl_certificate     $CERT_DIR/api.sancaka.monosolusi.com/fullchain.pem;|" /etc/nginx/conf.d/reverse-proxy.conf
-# sed -i "s|ssl_certificate_key /etc/ssl/private/ssl-cert-snakeoil.key;|ssl_certificate_key $CERT_DIR/api.sancaka.monosolusi.com/privkey.pem;|" /etc/nginx/conf.d/reverse-proxy.conf
-
 sed -i "s|ssl_certificate     /etc/ssl/certs/ssl-cert-snakeoil.pem;|ssl_certificate     $CERT_DIR/grafana.tokosumatra.monosolusi.com/fullchain.pem;|" /etc/nginx/conf.d/reverse-proxy.conf
 sed -i "s|ssl_certificate_key /etc/ssl/private/ssl-cert-snakeoil.key;|ssl_certificate_key $CERT_DIR/grafana.tokosumatra.monosolusi.com/privkey.pem;|" /etc/nginx/conf.d/reverse-proxy.conf
 
