5813b2daa319aa4327f135761e8143c1b282651a
Frans Siswanto <franssiswanto@gmail.com>
2025-02-03 09:26:24 +0700

    fix: updates the entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+set -e
+
 # Obtain certificates (nginx needs to be running)
 certbot certonly --standalone \
   -d sancaka-api.monosolusi.com  \
-  -d tokosumatra.monosolusi.com \
   --non-interactive \
   --agree-tos \
   --email frans@monosolusi.com
@@ -12,8 +13,20 @@ certbot certonly --standalone \
 sed -i "s/ssl_certificate     /etc/ssl/certs/ssl-cert-snakeoil.pem;/ssl_certificate     /etc/letsencrypt/live/sancaka-api.monosolusi.com/fullchain.pem;/" /etc/nginx/conf.d/reverse-proxy.conf
 sed -i "s/ssl_certificate_key /etc/ssl/private/ssl-cert-snakeoil.key;/ssl_certificate_key /etc/letsencrypt/live/sancaka-api.monosolusi.com/privkey.pem;/" /etc/nginx/conf.d/reverse-proxy.conf
 
+certbot certonly --standalone \
+  -d tokosumatra.monosolusi.com \
+  --non-interactive \
+  --agree-tos \
+  --email frans@monosolusi.com
+
 sed -i "s/ssl_certificate     /etc/ssl/certs/ssl-cert-snakeoil.pem;/ssl_certificate     /etc/letsencrypt/live/tokosumatra.monosolusi.com/fullchain.pem;/" /etc/nginx/conf.d/reverse-proxy.conf
 sed -i "s/ssl_certificate_key /etc/ssl/private/ssl-cert-snakeoil.key;/ssl_certificate_key /etc/letsencrypt/live/tokosumatra.monosolusi.com/privkey.pem;/" /etc/nginx/conf.d/reverse-proxy.conf
+
+if [ ! -f "/etc/letsencrypt/live/sancaka-api.monosolusi.com/fullchain.pem" ] || \
+   [ ! -f "/etc/letsencrypt/live/tokosumatra.monosolusi.com/fullchain.pem" ]; then
+  echo "ERROR: SSL certificates not generated!"
+  exit 1
+fi
 
 # Start nginx
 nginx -g 'daemon off;'
