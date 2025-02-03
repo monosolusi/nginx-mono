166115ef3474d86488bc8afe9ffb493f5fd0d4c9
Frans Siswanto <franssiswanto@gmail.com>
2025-02-03 09:09:52 +0700

    feat: adding grafana and loki for tokosumatra

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,8 @@ certbot certonly --standalone \
 sed -i "s/ssl_certificate     /etc/ssl/certs/ssl-cert-snakeoil.pem;/ssl_certificate     /etc/letsencrypt/live/sancaka-api.monosolusi.com/fullchain.pem;/" /etc/nginx/conf.d/reverse-proxy.conf
 sed -i "s/ssl_certificate_key /etc/ssl/private/ssl-cert-snakeoil.key;/ssl_certificate_key /etc/letsencrypt/live/sancaka-api.monosolusi.com/privkey.pem;/" /etc/nginx/conf.d/reverse-proxy.conf
 
+sed -i "s/ssl_certificate     /etc/ssl/certs/ssl-cert-snakeoil.pem;/ssl_certificate     /etc/letsencrypt/live/tokosumatra.monosolusi.com/fullchain.pem;/" /etc/nginx/conf.d/reverse-proxy.conf
+sed -i "s/ssl_certificate_key /etc/ssl/private/ssl-cert-snakeoil.key;/ssl_certificate_key /etc/letsencrypt/live/tokosumatra.monosolusi.com/privkey.pem;/" /etc/nginx/conf.d/reverse-proxy.conf
+
 # Start nginx
 nginx -g 'daemon off;'
