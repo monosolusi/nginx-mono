a6ae34acb176b27207e0611d73e022df77bf8e27
Frans Siswanto <franssiswanto@gmail.com>
2025-02-03 09:46:27 +0700

    feat: move grafana and loki to subdomain

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,16 +14,26 @@ sed -i "s|ssl_certificate     /etc/ssl/certs/ssl-cert-snakeoil.pem;|ssl_certific
 sed -i "s|ssl_certificate_key /etc/ssl/private/ssl-cert-snakeoil.key;|ssl_certificate_key /etc/letsencrypt/live/sancaka-api.monosolusi.com/privkey.pem;|" /etc/nginx/conf.d/reverse-proxy.conf
 
 certbot certonly --standalone \
-  -d tokosumatra.monosolusi.com \
+  -d grafana.tokosumatra.monosolusi.com \
   --non-interactive \
   --agree-tos \
   --email frans@monosolusi.com
 
-sed -i "s|ssl_certificate     /etc/ssl/certs/ssl-cert-snakeoil.pem;|ssl_certificate     /etc/letsencrypt/live/tokosumatra.monosolusi.com/fullchain.pem;|" /etc/nginx/conf.d/reverse-proxy.conf
-sed -i "s|ssl_certificate_key /etc/ssl/private/ssl-cert-snakeoil.key;|ssl_certificate_key /etc/letsencrypt/live/tokosumatra.monosolusi.com/privkey.pem;|" /etc/nginx/conf.d/reverse-proxy.conf
+sed -i "s|ssl_certificate     /etc/ssl/certs/ssl-cert-snakeoil.pem;|ssl_certificate     /etc/letsencrypt/live/grafana.tokosumatra.monosolusi.com/fullchain.pem;|" /etc/nginx/conf.d/reverse-proxy.conf
+sed -i "s|ssl_certificate_key /etc/ssl/private/ssl-cert-snakeoil.key;|ssl_certificate_key /etc/letsencrypt/live/grafana.tokosumatra.monosolusi.com/privkey.pem;|" /etc/nginx/conf.d/reverse-proxy.conf
+
+certbot certonly --standalone \
+  -d loki.tokosumatra.monosolusi.com \
+  --non-interactive \
+  --agree-tos \
+  --email frans@monosolusi.com
+
+sed -i "s|ssl_certificate     /etc/ssl/certs/ssl-cert-snakeoil.pem;|ssl_certificate     /etc/letsencrypt/live/loki.tokosumatra.monosolusi.com/fullchain.pem;|" /etc/nginx/conf.d/reverse-proxy.conf
+sed -i "s|ssl_certificate_key /etc/ssl/private/ssl-cert-snakeoil.key;|ssl_certificate_key /etc/letsencrypt/live/loki.tokosumatra.monosolusi.com/privkey.pem;|" /etc/nginx/conf.d/reverse-proxy.conf
 
 if [ ! -f "/etc/letsencrypt/live/sancaka-api.monosolusi.com/fullchain.pem" ] || \
-   [ ! -f "/etc/letsencrypt/live/tokosumatra.monosolusi.com/fullchain.pem" ]; then
+   [ ! -f "/etc/letsencrypt/live/grafana.tokosumatra.monosolusi.com/fullchain.pem" ] || \
+   [ ! -f "/etc/letsencrypt/live/loki.tokosumatra.monosolusi.com/fullchain.pem" ]; then
   echo "ERROR: SSL certificates not generated!"
   exit 1
 fi
